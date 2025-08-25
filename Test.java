9b91fc6271513060a083d876d37efe2300c6c1e4
patil0507 <116193386+patil0507@users.noreply.github.com>
2025-08-25 23:37:27 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-public class HelloJenkins {
+public class Hello {
     public static void main(String[] args) {
         System.out.println("********************************************");
         System.out.println("Hello, This is my first Jenkins Job project!");
